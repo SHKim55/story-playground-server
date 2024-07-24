68ab35f1d9f16e27cc110454c7ac78060b42466d
Seonho Kim <5061ksh@naver.com>
2024-07-25 00:17:27 +0900

    Initial commit

---
+++ src/main/java/com/softgallery/story_playground_server/StoryPlaygroundServerApplication.java
@@ -0,0 +1,13 @@
+package com.softgallery.story_playground_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryPlaygroundServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryPlaygroundServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softgallery/story_playground_server/StoryPlaygroundServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softgallery.story_playground_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryPlaygroundServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
